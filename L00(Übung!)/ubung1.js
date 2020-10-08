4c4a1c14576fdf49373c8eabb36e1873230dc42f
halisco <48839469+halisco@users.noreply.github.com>
2020-10-08 19:35:55 +0200

    Übung00

---
+++ L00(Übung!)/ubung1.js
@@ -0,0 +1,7 @@
+"use strict";
+var person = prompt("Please enter your name");
+if (person != null) {
+    document.getElementById("name").innerHTML =
+        "Hey " + person + ", was geht ab? ;)";
+}
+//# sourceMappingURL=ubung1.js.map
---
+++ L00(Übung!)/ubung1.ts
@@ -0,0 +1,6 @@
+var person = prompt("Please enter your name");
+
+if (person != null) {
+  document.getElementById("name").innerHTML =
+  "Hey " + person + ", was geht ab? ;)";
+}
