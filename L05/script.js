f50a265ba13457f268f7c1c476464ca691277ba3
halisco <haliscan97@hotmail.de>
2020-11-14 22:49:03 +0100

    L05

--- L05/script.js
+++ L05/script.js
@@ -1,2 +1,20 @@
 "use strict";
+var L05;
+(function (L05) {
+    window.addEventListener("load", handleload);
+    function handleload(_event) {
+        let form = document.querySelector("div#form");
+        form.addEventListener("change", handleChange);
+    }
+    function handleChange(_event) {
+        let order = document.querySelector("div#order");
+        order.innerHTML = "";
+        let formular = new FormData(document.forms[0]);
+        for (let entry of formular) {
+            let object = document.querySelector("[value='" + entry[1] + "']");
+            let price = Number(object.getAttribute("price"));
+            order.innerHTML += object.getAttribute("value") + "   " + price + "€";
+        }
+    }
+})(L05 || (L05 = {}));
 //# sourceMappingURL=script.js.map
--- L05/script.ts
+++ L05/script.ts
@@ -1,3 +1,23 @@
-namespace L03 {
+namespace L05 {
+window.addEventListener("load", handleload);
+
+function handleload(_event: Event): void {
+    let form: HTMLDivElement = <HTMLDivElement>document.querySelector("div#form");
+    form.addEventListener("change", handleChange);
+}
+
+function handleChange(_event: Event): void {
+    let order: HTMLDivElement = <HTMLDivElement>document.querySelector("div#order");
+    order.innerHTML = "";
+
+    let formular: FormData = new FormData(document.forms[0]);
+    for (let entry of formular) {
+        let object: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" + entry[1] + "']");
+        let price: number = Number(object.getAttribute("price"));
+
+        order.innerHTML += object.getAttribute("value") + "   " + price + "€";
+    }
+}
+
     
 }
